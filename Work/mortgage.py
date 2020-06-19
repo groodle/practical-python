36b2e830544717e821e1f7b3843daa0835451295
Elliot Cooper <me@elliotcooper.com>
2020-06-19 14:34:34 +0100

    solved for 1.9 and 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,16 +8,19 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+extra_payment_start_month = 48
+extra_payment_end_month = 60
 extra_payment = 1000
-extra_payment_months = 12
 
 while principal > 0:
-    while month < extra_payment_months:
+    while month < extra_payment_start_month and month > extra_payment_end_month:
         principal = principal * (1+rate/12) - (payment + extra_payment)
         total_paid = total_paid + payment + extra_payment
+        print(month, total_paid, principal)
         month = month + 1
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
+    print(month, total_paid, principal)
     month = month + 1
 
 print('Months =', month)
